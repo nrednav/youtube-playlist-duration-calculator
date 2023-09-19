8d01395e6957b765bfe7d8ea61eafd491fbe5401
Californ1a <rk_california@yahoo.com>
2023-09-18 20:09:14 -0400

    rewrite

---
+++ src/content.js
@@ -0,0 +1,35 @@
+import {
+  configurePage,
+  setupPlaylistObserver,
+  setupEventListeners,
+  getVideos,
+  getTimestamps,
+  formatTimestamp,
+  createPlaylistSummary,
+  addSummaryToPage,
+  pollPlaylistReady,
+} from './library';
+
+const start = () => {
+  configurePage();
+  setupPlaylistObserver(start);
+  setupEventListeners(start);
+  const videos = getVideos();
+  const timestamps = getTimestamps(videos);
+  const totalDurationInSeconds = timestamps.reduce((a, b) => a + b);
+  const playlistDuration = formatTimestamp(totalDurationInSeconds);
+  const playlistSummary = createPlaylistSummary({
+    timestamps,
+    playlistDuration,
+  });
+  addSummaryToPage(playlistSummary);
+};
+
+// Entry-point
+if (document.readyState !== 'loading') {
+  pollPlaylistReady(start);
+} else {
+  document.addEventListener('DOMContentLoaded', () => {
+    pollPlaylistReady(start);
+  });
+}
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+export default defineConfig({
+  plugins: [
+    webExtension({
+      manifest: generateManifest,
+      watchFilePaths: ["package.json", "src/manifest.json"],
+      browser: process.env.TARGET_BROWSER || "chrome",
+      webExtConfig: {
+        startUrl: ["https://www.youtube.com/playlist?list=PLaAVDbMg_XArcet5lwcRo12Fh9JrGKydh"],
+      }
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  }
+});
