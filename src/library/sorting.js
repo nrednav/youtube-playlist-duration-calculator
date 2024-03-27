da07c452c14680ab0b43e69efd5b53e35b883763
vandern rodrigues <nrednav@gmail.com>
2024-03-27 21:10:56 +0400

    feat: implement sort by upload date

--- src/library/sorting.js
+++ src/library/sorting.js
@@ -158,6 +158,61 @@ class SortByViewsStrategy {
   }
 }
 
+class SortByUploadDateStrategy {
+  /**
+   * Sorts a list of videos by their upload date
+   * @param {Array<Element>} videos
+   * @param {"asc" | "desc"} sortOrder
+   * @returns {Array<Element>}
+   */
+  sort(videos, sortOrder) {
+    return Array.from(videos)
+      .slice(0, 100)
+      .sort((videoA, videoB) => {
+        const videoInfoA = videoA.querySelector(
+          "yt-formatted-string#video-info"
+        );
+        const videoInfoB = videoB.querySelector(
+          "yt-formatted-string#video-info"
+        );
+
+        const secondsA = this.extractUploadDateAsSeconds(videoInfoA);
+        const secondsB = this.extractUploadDateAsSeconds(videoInfoB);
+
+        if (sortOrder === "asc") {
+          return secondsA - secondsB;
+        }
+
+        if (sortOrder === "desc") {
+          return secondsB - secondsA;
+        }
+      });
+  }
+
+  /**
+   * Extracts the upload date as seconds from a video info element
+   * @param {Element} videoInfo
+   * @returns {number}
+   */
+  extractUploadDateAsSeconds(videoInfo) {
+    const secondsByUnit = {
+      day: 1 * 86400,
+      week: 7 * 86400,
+      month: 30 * 86400,
+      year: 365 * 86400
+    };
+
+    const uploadDateElement = videoInfo.children[2];
+    const uploadDateRegex = /(\d+) (\w+) ago/;
+    const [value, unit] = uploadDateElement.textContent
+      .toLowerCase()
+      .match(uploadDateRegex)
+      .slice(1);
+    const normalizedUnit = unit.endsWith("s") ? unit.slice(0, -1) : unit;
+    return parseFloat(value) * secondsByUnit[normalizedUnit];
+  }
+}
+
 /**
  * Generates an object containing information about each supported sort type
  * @returns {Object}
@@ -194,6 +249,14 @@ const generateSortTypes = () => ({
       desc: "Views (Most)"
     },
     strategy: SortByViewsStrategy
+  },
+  uploadDate: {
+    enabled: videoHasElement("yt-formatted-string#video-info"),
+    label: {
+      asc: "Upload Date (Newest)",
+      desc: "Upload Date (Oldest)"
+    },
+    strategy: SortByUploadDateStrategy
   }
 });
 
