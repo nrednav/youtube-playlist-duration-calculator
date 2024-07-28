c91705c2f9d1dcc62be4b4a494bc8edc8ba5b479
pifopi <dottel.gael@gmail.com>
2024-07-28 16:54:34 +0200

    fix youtube premium location when fullscreen

--- src/shared/data/element-selectors.js
+++ src/shared/data/element-selectors.js
@@ -12,7 +12,7 @@ export const elementSelectors = {
   playlistMetadata: {
     old: "ytd-playlist-sidebar-renderer #items",
     new: ".immersive-header-content .metadata-action-bar",
-    youtubePremium: "yt-content-metadata-view-model"
+    youtubePremium: ".yt-flexible-actions-view-model-wiz__action-row"
   },
   video: "ytd-playlist-video-renderer",
   playlist: "ytd-playlist-video-list-renderer #contents",
